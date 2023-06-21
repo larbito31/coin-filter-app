83b30ea234d9737cc27907e40a3a51e8220b128e
larbito31 <boukhobza.larbi31@gmail.com>
2023-06-22 01:47:33 +0200

    create a GET req

--- app/api/coins/route.js
+++ app/api/coins/route.js
@@ -1,6 +1,17 @@
 import { NextResponse } from "next/server";
 
 async function fetchCoins() {
-    const response = await fetch()
-
+    const response = await fetch('https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0',  { // alt + z 
+        "method": "GET",
+        "headers": {
+            'X-RapidAPI-Key': '74664fb8c1msh47ce93b42c197bap1ed5e4jsn018fb2139f29',
+		    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'    
+        }
+    }) 
+        const coins = await response.json()
+        return coins
+}
+export async function GET(request) {
+    const coins = await fetchCoins()
+    return NextResponse.json(coins)
 }
