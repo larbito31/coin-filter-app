36d330ad7a6f1aaadf8cfae40165e61aa79f582b
larbito31 <boukhobza.larbi31@gmail.com>
2023-06-22 16:28:00 +0200

    create Coins Component

--- app/page.js
+++ app/page.js
@@ -1,6 +1,27 @@
+"use client"
+
+import { useState, useEffect } from "react"
+import Coins from "./components/Coins"
+
 
 export default function Home() {
+  const [coins, setCoins] = useState([])
+
+  useEffect(()=> {
+    const getCoins = async () => {
+      const response = await fetch('/api/coins')
+      const coins = await response.json()
+      setCoins(coins.data.coins)
+    }
+
+    getCoins()
+  }, [])  // just one time like componentDidmount,[]
+
   return (
-    <div> home Page</div>
+    <div className="text-center"> 
+      <h1 className="font-bold text-4xl mt-14"> Crypto coins </h1>
+      <br></br>
+      <Coins coins={coins}/>
+    </div>
   )
 }
